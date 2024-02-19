48c4c37828d6b2f50720646b3b68524fff08bfd5
Your Name <you@example.com>
2024-02-19 17:50:11 +0000

    up

---
+++ models/amenity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """Represents an Amenity for sql.
+
+    Inherits from SQLAlchemy Base
+
+    Attributes:
+        __tablename__ (str): name
+        name (sqlalchemy String): amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
