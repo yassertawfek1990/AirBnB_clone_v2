ca10f1fb88cb3598bb3ca711d1b6c98f15848e19
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-20 20:22:08 +0200

    c

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,19 @@
+-- Createwith:
+--   Databadb.
+--   Userlocalhost.
+--   Grantshbnb_dev_db.
+--   Grantsperformance_schema.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
